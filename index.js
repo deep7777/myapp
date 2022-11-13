6ca809f2cb08786ea29e95e3cbb8ee3a528abc11
Deepak Yadav <deepak@appnexio.com>
2022-11-14 01:48:56 +0530

    add new ip

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 var mysql = require('mysql')
-let host = process.env.DB_ENDPOINT || "localhost";
+let host = process.env.DB_ENDPOINT || "103.191.208.50";
 let user = process.env.DB_USER || "gywuzdgj_deepak";
-let password = process.env.DB_PASSWORD || "@32zndr84s";
+let password = process.env.DB_PASSWORD || "@32zndr84";
 let database = process.env.DB_NAME || "gywuzdgj_deepak";
 var connection = mysql.createConnection({
   host,
@@ -21,6 +21,7 @@ app.get('/', (req, res) => {
   connection.connect((err) => {
     if (err) {
       console.log(err)
+      let message = err.message+'-'+Math.random();
       res.send(err.message)
       return
     }else {
